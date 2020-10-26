df7291f292d94150f937d36b70988461e8c75dbc
IAmMum-sAsh <56122911+IAmMum-sAsh@users.noreply.github.com>
2020-10-26 13:13:24 +0300

    lab12

--- lab12 - Vitukhina/src/ru/mirea/ikbo_01_19/lab12/Test.java
+++ lab12 - Vitukhina/src/ru/mirea/ikbo_01_19/lab12/Test.java
@@ -7,7 +7,7 @@ import java.util.regex.Pattern;
 public class Test {
     public static void main(String[] args){
         Scanner scan = new Scanner(System.in);
-        Pattern pat = Pattern.compile("^[a-z[A-Z][0-9][_]](?=.*[0-9])(?=.*[a-z])(?=.*[A-Z]).{8,}$");
+        Pattern pat = Pattern.compile("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?!.*[\\x{0020}-\\x{002F}\\x{003A}-\\x{0040}\\x{005B}-\\x{005E}\\x{0060}\\x{007B}-\\x{007E}]).{8,}$");
 
         System.out.println("Введите пароль для проверки или 1227 для завершения");
         String pass = "";
@@ -18,3 +18,13 @@ public class Test {
         }
     }
 }
+/*
+Kot1k1SpasutM1r
+kot1k1spasutm1r
+KotikiSpasutMir
+KOT1K1SPASUTM1R
+33!Korovi
+33_Korovi
+K0r0tk
+
+ */
